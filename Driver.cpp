eae5a8c302be1a3e203373abe306fc5bace5a732
Bryan <TheBryanD@users.noreply.github.com>
2019-04-11 02:20:53 -0500

    Got a succesful test from the Program. The random number gen isn't random enough yet.

--- Driver.cpp
+++ Driver.cpp
@@ -3,10 +3,70 @@
 */
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <cstdlib>
+#include "Locations.h"
 
 using namespace std;
 
+int randomNumGen(int);
+
+Location getLocation(Location*, int);
+
 int main()
 {
-  Location school("school");
+  int LOCATION_SIZE = 2;
+  Location locations[LOCATION_SIZE];
+
+  Location school("School");
+  school.setRole("Teacher");
+  school.setRole("Student");
+  school.setRole("Janitor");
+  school.setRole("Gym Coach");
+  school.setRole("Freshman");
+  school.setRole("Student President");
+  school.setRole("Principal");
+  school.setRole("Visiter");
+  school.setRole("Teacher");
+  school.setRole("Visiter");
+  locations[0] = school;
+
+  Location busStop("Bus Stop");
+  busStop.setRole("Bus Driver");
+  busStop.setRole("Student");
+  busStop.setRole("Constrution Worker");
+  busStop.setRole("Single Mother");
+  busStop.setRole("Upset Obese Child");
+  busStop.setRole("Busy Business Man");
+  busStop.setRole("Deilan");
+  busStop.setRole("Chester");
+  busStop.setRole("Baby man");
+  busStop.setRole("Bryan");
+  locations[1] = busStop;
+
+  Location yourLocation = getLocation(locations, randomNumGen(LOCATION_SIZE));
+
+  cout << "Your random number is: " << randomNumGen(LOCATION_SIZE) << endl;
+  cout << "The location is :" << yourLocation.getName() << endl;
+  cout << "Your role is :" << yourLocation.getRole(randomNumGen(10 )) << endl;
+}
+
+//Creates a random number and returns it.
+int randomNumGen(int length)
+{
+  //Error: isn't completely random
+  srand((unsigned)time(0));
+    int random_integer;
+    random_integer = (rand()%length);
+    return random_integer;
+}
+
+/*
+  Purpose: returns a random location.
+  Arguments: accepts an array of locations, and random integer.
+  Returns: Location object
+*/
+Location getLocation(Location* locations, int random_integer)
+{
+  return locations[random_integer];
 }
--- Locations.h
+++ Locations.h
@@ -3,26 +3,54 @@
 */
 #ifndef LOCATIONS_H
 #define LOACTIONS_H
+#include <iostream>
 #include <string>
 using namespace std;
 
 class Location {
 private:
   string name;
-  string[10] roles;
+  string roles[10];
+  int length = 0;
 
 public:
+  //default constructor
+  Location()
+  { }
+
+  //Constructor
   Location(string n)
   {
     name = n;
   }
-  getName()
+
+  //Get the name of the location
+  string getName()
   {
     return name;
   }
-  setRole(string r)
+
+  //set the roles in the array
+  void setRole(string r)
   {
-    roles[] = r;
+    if(length <= 10)
+    {
+      roles[length] = r;
+      length++;
+    }
+    else
+    {
+      cout << "Array out of bounds." << endl;
+    }
+  }
+
+  //Get the role you are using
+  string getRole(int random_integer)
+  {
+    if(random_integer <= length)
+      return roles[random_integer];
+    else
+      return "Invalid, see getRole (Out of bounds)";
   }
 };
 
